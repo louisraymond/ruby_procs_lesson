95e606be68a88b4bdb42d7be2d633b0931ac9060
Louis Raymond <louis@louisraymond.com>
2025-02-24 13:42:05 +0000

    Alter files

--- procs.rb
+++ procs.rb
@@ -1,7 +1,5 @@
 class ProcChallenge
     def self.define_procs
-        double = Proc.new { |x| x * 2 }
-        triple = Proc.new { |x| x * 3 }
 
         # DO NOT EDIT
         return double, triple
--- test/double_triple_test.rb
+++ test/double_triple_test.rb
@@ -1,22 +1,30 @@
-require "minitest/autorun"
+require "test_helper"
 require_relative "../procs"
 
 class DoubleTripleTest < Minitest::Test
   def test_double_is_a_proc
-    assert_kind_of Proc, ProcChallenge.return_proc(:double), "Expected 'double' to be a Proc"
+    double = ProcChallenge.return_proc(:double)
+    assert double.is_a?(Proc), "Expected 'double' to be a Proc, but got #{double.class}"
+    assert_equal 4, double.call(2), "Expected double(2) to return 4"
   end
 
   def test_triple_is_a_proc
-    assert_kind_of Proc, ProcChallenge.return_proc(:triple), "Expected 'triple' to be a Proc"
+    triple = ProcChallenge.return_proc(:triple)
+    assert triple.is_a?(Proc), "Expected 'triple' to be a Proc, but got #{triple.class}"
+    assert_equal 6, triple.call(2), "Expected triple(2) to return 6"
   end
 
   def test_double_behavior
-    assert_equal 4, ProcChallenge.return_proc(:double).call(2)
-    assert_equal 10, ProcChallenge.return_proc(:double).call(5)
+    double = ProcChallenge.return_proc(:double)
+    assert_equal 10, double.call(5), "double(5) should return 10"
   end
 
   def test_triple_behavior
-    assert_equal 6, ProcChallenge.return_proc(:triple).call(2)
-    assert_equal 15, ProcChallenge.return_proc(:triple).call(5)
+    triple = ProcChallenge.return_proc(:triple)
+    assert_equal 15, triple.call(5), "triple(5) should return 15"
+  end
+
+  def test_invalid_proc
+    assert_raises(NoMethodError) { ProcChallenge.return_proc(:quadruple).call(5) }
   end
 end
